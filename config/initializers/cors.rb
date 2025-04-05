881c167ef8dd3e1a3261e718e9f55842e183b5d1
Ajbakaric <bakaricaustin@yahoo.com>
2025-04-05 14:37:29 -0600

    cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://playful-croissant-d2c181.netlify.app',
+        /\Ahttps:\/\/[a-z0-9\-]+--playful-croissant-d2c181\.netlify\.app\z/
+
 
     resource '*',
       headers: :any,
