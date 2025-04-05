dddff7dc0fc01fae824ea320bd6503e05665cefd
Ajbakaric <bakaricaustin@yahoo.com>
2025-04-05 16:21:27 -0600

    application_controller.rb update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,26 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
   include ActionController::Cookies
   include ActionController::RequestForgeryProtection
-  include ActionController::MimeResponds
+
+  protect_from_forgery with: :null_session
 
   respond_to :json
-
-  # Protect from CSRF attacks
-  protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
+  # Allow extra params for Devise user actions
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar])
+  end
+
+  # Override Devise method to handle unauthorized access in API
+  def authenticate_user!(opts = {})
+    unless user_signed_in?
+      render json: { error: "Unauthorized" }, status: :unauthorized
+    end
   end
 end
