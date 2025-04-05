2dcfa5e1009ef3ea1a8e706babdb3621b43fa52b
Ajbakaric <bakaricaustin@yahoo.com>
2025-04-05 15:45:48 -0600

    roll back after this if not work

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,13 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://playful-croissant-d2c181.netlify.app',
-        /\Ahttps:\/\/[a-z0-9\-]+--playful-croissant-d2c181\.netlify\.app\z/
-
+    origins 'https://playful-croissant-d2c181.netlify.app' # your Netlify app
 
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Authorization'],
-      credentials: true
+      credentials: true # if you're using cookies/sessions
   end
 end
+
