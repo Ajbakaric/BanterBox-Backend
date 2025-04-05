5ea40720793a103b41a5f9405e775d8ccc68f787
Ajbakaric <bakaricaustin@yahoo.com>
2025-04-05 10:43:39 -0600

    fixed cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,12 +6,16 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       expose: ['Authorization'],
-      credentials: true  # ← ✅ THIS LINE allows cookies + Authorization headers
+      credentials: true
+  end
+
+  allow do
+    origins 'https://playful-croissant-d2c181.netlify.app'
 
     resource '/cable',
       headers: :any,
       methods: [:get, :post, :options],
       expose: ['Authorization'],
-      credentials: true  # ← ✅ SAME HERE
+      credentials: true
   end
 end
